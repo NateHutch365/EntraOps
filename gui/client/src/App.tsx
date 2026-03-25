573931353a61088713f0df81d7e79ebfecd1d6de
Nate Hutchinson <nate.hutch@hotmail.com>
2026-03-25 16:14:38 -0700

    feat(04-02): install sonner and mount Toaster in App

--- gui/client/src/App.tsx
+++ gui/client/src/App.tsx
@@ -5,18 +5,22 @@ import { ObjectBrowser } from '@/pages/ObjectBrowser';
 import { ObjectDetail } from '@/pages/ObjectDetail';
 import { TemplatesPage } from '@/pages/TemplatesPage';
 import { RunCommandsPage } from '@/pages/RunCommandsPage';
+import { Toaster } from '@/components/ui/sonner';
 
 export default function App() {
   return (
-    <Routes>
-      <Route element={<AppShell />}>
-        <Route index element={<Dashboard />} />
-        <Route path="objects" element={<ObjectBrowser />} />
-        <Route path="objects/:objectId" element={<ObjectDetail />} />
-        <Route path="templates" element={<TemplatesPage />} />
-        <Route path="run" element={<RunCommandsPage />} />
-      </Route>
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route element={<AppShell />}>
+          <Route index element={<Dashboard />} />
+          <Route path="objects" element={<ObjectBrowser />} />
+          <Route path="objects/:objectId" element={<ObjectDetail />} />
+          <Route path="templates" element={<TemplatesPage />} />
+          <Route path="run" element={<RunCommandsPage />} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+      <Toaster />
+    </>
   );
 }
